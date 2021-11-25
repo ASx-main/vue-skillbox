81cf64fddc6c25ec0d3dd7c491db52ad2bd1610b
Anastasiya S <as@Anastasiyas-MacBook-Air.local>
2021-11-25 17:16:36 +0300

    home work 3.6 (5)

---
+++ src/data/products.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    title: 'Серьги детские TALANT Яблоко из золота с фианитами',
+    price: 5616,
+    img: './img/earrings.webp',
+  },
+  {
+    title: 'Серьги-конго TALANT из золота',
+    price: 12320,
+    img: './img/earrings2.webp',
+  },
+  {
+    title: 'Серьги TALANT из золота с фианитами',
+    price: 17152,
+    img: './img/earrings3.webp',
+  },
+  {
+    title: 'Серьги-пусеты Цветок из золота с бриллиантами',
+    price: 7220,
+    img: './img/earrings4.webp',
+  },
+  {
+    title: 'Серьги-продевки из золота с фианитами',
+    price: 5248,
+    img: './img/earrings5.webp',
+  },
+  {
+    title: 'Серьги-пусеты TALANT из золота с кристаллами SWAROVSKI',
+    price: 7440,
+    img: './img/earrings6.webp',
+  },
+  {
+    title: 'Серьги детские из золота со шпинелью и фианитами',
+    price: 5680,
+    img: './img/earrings7.webp',
+  },
+  {
+    title: 'Серьги TALANT из золота с аметистами и фианитами',
+    price: 4980,
+    img: './img/earrings8.webp',
+  },
+  {
+    title: 'Серьги из золота с агатами и бриллиантами',
+    price: 12830,
+    img: './img/earrings9.webp',
+  },
+  {
+    title: 'Серьги-пусеты Звезда из золота с фианитами',
+    price: 5312,
+    img: './img/earrings10.webp',
+  },
+  {
+    title: 'Серьги детские Цветок из золота со шпинелью',
+    price: 6176,
+    img: './img/earrings11.webp',
+  },
+  {
+    title: 'Серьги-пусеты TALANT из золота с топазами и фианитами',
+    price: 6450,
+    img: './img/earrings12.webp',
+  },
+  {
+    title: 'Серьги из золота с раух-топазами и фианитами',
+    price: 11080,
+    img: './img/earrings13.webp',
+  },
+  {
+    title: 'Серьги из золота с аметринами и фианитами',
+    price: 9700,
+    img: './img/earrings14.webp',
+  },
+  {
+    title: 'Серьги-продевки SOKOLOV из золота с фианитами',
+    price: 8000,
+    img: './img/earrings15.webp',
+  },
+];
